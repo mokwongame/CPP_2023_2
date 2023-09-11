844dff9007476451e61807c3ff909befe7475f1e
mokwongame <ictplusud@gmail.com>
2023-09-11 14:45:42 +0900

    CoinTossGame 프로젝트 추가

---
+++ CoinTossGame/CoinTossGame.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // VS 헤더 파일 폴더 검색
+#include "LibConsole.h" // 현재 폴더 검색
+#include "LibGameTool.h"
+
+// 출력자료형 함수명(입력 선언, ...) {...}
+void main(void) // void(없는)
+{}
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "MgHead 추가"
+git commit -m "CoinTossGame 프로젝트 추가"
 git push -u origin master
