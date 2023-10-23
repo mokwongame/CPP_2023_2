9f568f9d66879b06fa3dfc6a5436b62316bae461
mokwongame <ictplusud@gmail.com>
2023-10-23 14:47:40 +0900

    생성자와 소멸자 추가

--- TestClassDef/MyCircle.h
+++ TestClassDef/MyCircle.h
@@ -11,6 +11,16 @@ class MyCircle // MyCircle이 클래스명
 {
 public: // public 그룹: 아래 있는 멤버는 모두 public
 	// 멤버 함수(method: 메소드, 방법)는 보통 public 그룹에 정의
+	// 생성자(constructor): 메소드명 == 클래스명; 클래스가 생성될 때(인스턴스가 만들어질 때) 자동적으로 호출되는 메소드(멤버 함수); 반환하는 자료형은 쓸 수 없음; 클래스를 쓰기 위한 기본적인 초기화 실행
+	MyCircle(void)
+	{
+		std::cout << "생성자 호출" << std::endl;
+	}
+	// 소멸자, 파괴자(destructor): 메소드명 == ~클래스명; 인스턴스가 소멸될 때 자동적으로 호출; 입력 자료형 X, 출력 자료형 X; 메모리 정리할 때 주로 사용
+	~MyCircle()
+	{
+		std::cout << "소멸자 호출" << std::endl;
+	}
 	// 프로퍼티 획득 함수(getter)
 	double getRad(void) const { return m_rad; } // 인라인 함수로 처리; 함수 입출력 정의 뒤 const(상수) 의미: 이 함수는 멤버 변수를 변경하지 않음(쓰지 않음)
 	double getCenterX(void) const { return m_x; }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "메소드에 추가한 const 설명"
+git commit -m "생성자와 소멸자 추가"
 git push -u origin master
